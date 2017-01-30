014581a00d96268dab9a4856fe86b4fe695445f8
olegoid <olegoid91@gmail.com>
2017-01-30 14:12:07 -0300

    XamarinProject spec skeleton

---
+++ fastlane_core/spec/xamarin_project_spec.rb
@@ -0,0 +1,48 @@
+describe FastlaneCore do
+  describe FastlaneCore::XamarinProject do
+    describe 'initialize' do
+      it 'init type, type_guids, nugets as empty arrays' do
+      end
+    end
+
+    describe 'ios?' do
+      it 'return true for iOS project' do
+      end
+
+      it 'return false for not iOS project' do
+      end
+    end
+
+    describe 'tvos?' do
+      it 'return true for tvOS project' do
+      end
+
+      it 'return false for not tvOS project' do
+      end
+    end
+
+    describe 'mac?' do
+      it 'return true for Mac project' do
+      end
+
+      it 'return false for not Mac project' do
+      end
+    end
+
+    describe 'android?' do
+      it 'return true for Android project' do
+      end
+
+      it 'return false for not Android project' do
+      end
+    end
+
+    describe 'test?' do
+      it 'return true for Nunit test library project' do
+      end
+
+      it 'return false for not Nunit test library project' do
+      end
+    end
+  end
+end
