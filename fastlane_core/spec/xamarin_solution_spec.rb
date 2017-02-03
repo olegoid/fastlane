32066379f4938963252f17a74a763fd79d90c54d
olegoid <olegoid91@gmail.com>
2017-02-03 00:19:30 -0300

    XamarinSolution: solution detection tests

--- fastlane_core/spec/xamarin_solution_spec.rb
+++ fastlane_core/spec/xamarin_solution_spec.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 describe FastlaneCore do
   describe FastlaneCore::XamarinSolution do
     describe 'solution file detection' do
@@ -9,32 +11,89 @@ describe FastlaneCore do
         end
       end
 
-      it 'raises error if solution file was not found' do
-
-      end
-
       it 'picks the only solution file present' do
+        within_a_temp_dir do |dir|
+          solution = "./Solution.sln"
+          FileUtils.touch(solution)
 
+          config = {}
+          FastlaneCore::XamarinSolution.detect_solutions(config)
+
+          expect(config[:xamarin_solution]).to eq(solution)
+        end
       end
 
       it 'prompts to select among multiple solution files' do
+        within_a_temp_dir do |dir|
+          solutions = [ "./FirstSolution.sln", "./SecondSolution.sln" ]
+          solutions.each { |solution| FileUtils.touch(solution) }
 
+          expect(FastlaneCore::XamarinSolution).to receive(:choose).and_return(solutions.last)
+          expect(FastlaneCore::XamarinSolution).not_to receive(:select_solution)
+
+          config = {}
+          FastlaneCore::XamarinSolution.detect_solutions(config)
+
+          expect(config[:xamarin_solution]).to eq(solutions.last)
+        end
       end
 
       it 'asks the user to specify a solution when none are found' do
+        within_a_temp_dir do |dir|
+          solution = "Solution.sln"
+          path = 'subdir'
 
+          FileUtils.mkdir_p(path)
+          FileUtils.touch(File.join(dir, path, solution))
+
+          expect(FastlaneCore::UI).to receive(:input).and_return(File.join(path, solution))
+
+          config = {}
+          FastlaneCore::XamarinSolution.detect_solutions(config)
+
+          expect(config[:xamarin_solution]).to eq(File.join(path, solution))
+        end
       end
 
       it 'explains when a provided path is not found' do
+        within_a_temp_dir do |dir|
+          solution = "Solution.sln"
+          path = 'subdir'
 
+          FileUtils.mkdir_p(path)
+          FileUtils.touch(File.join(dir, path, solution))
+
+          expect(FastlaneCore::UI).to receive(:input).and_return("something wrong")
+          expect(FastlaneCore::UI).to receive(:error).with(/Couldn't find/)
+          expect(FastlaneCore::UI).to receive(:input).and_return(File.join(path, solution))
+
+          config = {}
+          FastlaneCore::XamarinSolution.detect_solutions(config)
+
+          expect(config[:xamarin_solution]).to eq(File.join(path, solution))
+        end
       end
 
       it 'explains when a provided path is not valid' do
+        within_a_temp_dir do |dir|
+          solution = "Solution.sln"
+          path = 'subdir'
 
-      end
+          FileUtils.mkdir_p(path)
+          FileUtils.touch(File.join(dir, path, solution))
 
-      it "raises an exception if path was not found" do
+          otherFile = "Something.xyz"
+          FileUtils.touch(otherFile)
 
+          expect(FastlaneCore::UI).to receive(:input).and_return(otherFile)
+          expect(FastlaneCore::UI).to receive(:error).with(/Path must end with/)
+          expect(FastlaneCore::UI).to receive(:input).and_return(File.join(path, solution))
+
+          config = {}
+          FastlaneCore::XamarinSolution.detect_solutions(config)
+
+          expect(config[:xamarin_solution]).to eq(File.join(path, solution))
+        end
       end
     end
 
