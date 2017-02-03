c329f38da0d7b8551e28cb7b226e344cf17037bb
olegoid <olegoid91@gmail.com>
2017-02-03 00:20:18 -0300

    XamarinSolutionParser: user error tests

--- fastlane_core/spec/xamarin_solution_parser_spec.rb
+++ fastlane_core/spec/xamarin_solution_parser_spec.rb
@@ -1,12 +1,27 @@
 describe FastlaneCore do
   describe FastlaneCore::XamarinSolutionParser do
     describe 'parse' do
-      it 'raises error if solution file was not found' do
+      def within_a_temp_dir
+        Dir.mktmpdir do |dir|
+          FileUtils.cd(dir) do
+            yield dir if block_given?
+          end
+        end
+      end
 
+      it 'raises error if solution file was not found' do
+        expect do
+          FastlaneCore::XamarinSolutionParser.parse("/tmp/notHere123")
+        end.to raise_error "Could not find solution at path '/tmp/notHere123'"
       end
 
       it 'raises error if solution has no referenced projects' do
+        within_a_temp_dir do |dir|
+          solution = "Solution.sln"
+          FileUtils.touch(solution)
 
+          FastlaneCore::XamarinSolutionParser.parse(File.join(dir, solution))
+        end
       end
 
       it 'parses solution configurations' do
